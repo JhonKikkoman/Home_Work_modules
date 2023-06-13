b8941fadd3c5b7d2d916d664e148c20c7c77bd21
Piskun_Ivan <tw1st3r111@mail.ru>
2023-06-13 22:30:04 +0300

    task2

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,6 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
-            filename: "./index.html"
         }),
         new MiniCSSExtractPlugin({
             filename: "[name].css",
